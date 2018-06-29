571d67e064020ba49c11b2ba01398b33eebc16c6
zltunes <zltunes@icloud.com>
2018-06-29 16:15:51 +0800

    上传服务端代码；小程序登录、获取个人信息、获取过去一个月微信运动数据

---
+++ app/app.js
@@ -0,0 +1,87 @@
+//app.js
+App({
+  data: {
+    runData: [],
+  },
+  onLaunch: function () {
+    var that = this;
+
+    // 展示本地存储能力
+    var logs = wx.getStorageSync('logs') || []
+    logs.unshift(Date.now())
+    wx.setStorageSync('logs', logs)
+    var session_key
+
+    // 登录
+    wx.login({
+      success: res => {
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+        var appid = "wx8e16ce9067925ad7";
+        var secret = "4dbba79d45dc02202ad944e3036420e3";
+        if(res.code){
+          wx.request({
+            url: 'https://api.weixin.qq.com/sns/jscode2session?appid=' + appid + '&secret=' + secret + '&js_code=' + res.code + '&grant_type=authorization_code',
+            header: {
+              'content-type': 'json'
+            },
+            success: function(res){
+              session_key = res.data.session_key;
+
+            }
+          })
+          
+          wx.getWeRunData({
+            success(res) {
+
+              wx.request({
+                url: 'http://127.0.0.1:8081/decrypt',
+                data: {
+                  encryptedData: res.encryptedData,
+                  iv: res.iv,
+                  session_key: session_key
+                },
+                method: 'GET',
+                success: function (resDecrypt) {
+                  console.info(resDecrypt)
+                  // var runData = JSON.parse(resDecrypt.data.data)
+                  // console.info(runData);
+                  // if (runData.stepInfoList) {
+                  //   runData.stepInfoList = runData.stepInfoList.reverse()
+                  //   for (var i in runData.stepInfoList) {
+                  //     runData.stepInfoList[i].date = util.formatTime(new Date(runData.stepInfoList[i].timestamp * 1000))
+                  //   }
+                  //   that.setData({ runData: runData.stepInfoList });
+                  // }  
+                }
+              })
+
+            }
+          })
+        }
+      }
+    })
+    // 获取用户信息
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          wx.getUserInfo({
+            success: res => {
+              // 可以将 res 发送给后台解码出 unionId
+              this.globalData.userInfo = res.userInfo
+              
+              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+              // 所以此处加入 callback 以防止这种情况
+              if (this.userInfoReadyCallback) {
+                this.userInfoReadyCallback(res)
+              }
+            }
+          })
+        }
+      }
+    })
+  },
+  globalData: {
+    userInfo: null
+  }
+})
---
+++ server/server.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+
+var WXBizDataCrypt = require('./WXBizDataCrypt')
+
+var appId = 'wx8e16ce9067925ad7'
+ 
+app.get('/decrypt', function (req, res) {
+ 
+   var encryptedData = req.query.encryptedData
+   var iv = req.query.iv
+   var sessionKey = req.query.session_key
+
+   var pc = new WXBizDataCrypt(appId, sessionKey)
+   var response = pc.decryptData(encryptedData , iv)
+   console.log(response);
+   res.end(JSON.stringify(response));
+})
+ 
+var server = app.listen(8081, function () {
+ 
+  var host = '127.0.0.1'
+  var port = server.address().port
+ 
+  console.log("应用实例，访问地址为 http://%s:%s", host, port)
+ 
+})
